7a37f61af3cc80cceeb2d924fe5f33bb388504ed
miriloli <miriam3gga@gmail.com>
2024-03-22 19:47:12 +0100

    basic setup

---
+++ favorites/add_favorite.py
@@ -0,0 +1,35 @@
+import json
+import logging
+import os
+import time
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+
+def add_favorite(event, context):
+    data = json.loads(event['body'])
+    if 'org_id' or 'favourite_org_id' not in data:
+        logging.error("Validation Failed")
+        raise Exception("Couldn't create the todo item.")
+    
+    timestamp = str(time.time())
+
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+
+    item = {
+        'org_id': data['org_id'],
+        'favourite_org_id': data['favourite_org_id'],
+        'date': timestamp,
+        
+    }
+
+    # write the todo to the database
+    table.put_item(Item=item)
+
+    # create a response
+    response = {
+        "statusCode": 201,
+        "body": json.dumps(item)
+    }
+
+    return response

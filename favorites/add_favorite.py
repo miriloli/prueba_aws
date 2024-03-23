12a1490bcdbf9a589c4f042395c0065c82f7121d
miriloli <miriam3gga@gmail.com>
2024-03-23 20:38:50 +0100

    formating date, exception control

--- favorites/add_favorite.py
+++ favorites/add_favorite.py
@@ -1,36 +1,55 @@
 import json
 import logging
 import os
-import time
+import datetime
 import boto3
 
 dynamodb = boto3.resource('dynamodb')
 
 def add_favorite(event, context):
-    
-    data = json.loads(event['body'])
-    logging.error(data)
-    if 'org_id' not in data or 'favourite_org_id' not in data:
-        logging.error("Validation Failed")
-        raise Exception("Couldn't create the favorite item.")
-    
-    timestamp = str(time.time())
+    try:
+        data = json.loads(event['body'])
+        org_id = event['pathParameters']['id']
 
-    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
-    item = {
-        'org_id': data['org_id'],
-        'favourite_org_id': data['favourite_org_id'],
-        'date': timestamp,
+        if 'favourite_org_id' not in data:
+            logging.error("Validation Failed")
+            raise Exception("Couldn't create the favorite item.")
         
-    }
+        timestamp = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        
+        table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+        item = {
+            'org_id': org_id,
+            'favourite_org_id': data['favourite_org_id'],
+            'date': timestamp,
+        }
+        
+        table.put_item(Item=item)
+        
+        response = {
+            "statusCode": 201,
+            "body": json.dumps(item)
+        }
 
-    # write the todo to the database
-    table.put_item(Item=item)
+    except json.JSONDecodeError as e:
+        logging.error(f"Error al cargar los datos JSON: {e}")
+        response = {
+            "statusCode": 400,
+            "body": json.dumps({"error": "Error al cargar los datos JSON"})
+        }
 
-    # create a response
-    response = {
-        "statusCode": 201,
-        "body": json.dumps(item)
-    }
+    except KeyError as e:
+        logging.error(f"Error de clave faltante: {e}")
+        response = {
+            "statusCode": 400,
+            "body": json.dumps({"error": "Se esperaba un parámetro faltante"})
+        }
+
+    except Exception as e:
+        logging.error(f"Error desconocido: {e}")
+        response = {
+            "statusCode": 500,
+            "body": json.dumps({"error": "Se ha producido un error"})
+        }
 
     return response
--- favorites/get_favorites.py
+++ favorites/get_favorites.py
@@ -1,30 +1,37 @@
 import os
 import json
-
-from favorites import decimalencoder
 import boto3
 from boto3.dynamodb.conditions import Key
-dynamodb = boto3.resource('dynamodb')
+from favorites import decimalencoder
 
 def get_favorites(event, context):
-    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+    try:
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
 
-    
-    org_id = event['pathParameters']['id']
+        org_id = event['pathParameters']['id']
 
-    
-    result = table.query(
-        
-        KeyConditionExpression=Key('org_id').eq(org_id)
-    )
+        result = table.query(
+            KeyConditionExpression=Key('org_id').eq(org_id)
+        )
 
-    
-    items = result.get('Items', [])
+        items = result.get('Items', [])
 
-    
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(items, cls=decimalencoder.DecimalEncoder)
-    }
+        response = {
+            "statusCode": 200,
+            "body": json.dumps(items, cls=decimalencoder.DecimalEncoder)
+        }
+
+    except KeyError as e:
+        response = {
+            "statusCode": 400,
+            "body": json.dumps({"error": "Se esperaba un parámetro faltante"})
+        }
+
+    except Exception as e:
+        response = {
+            "statusCode": 500,
+            "body": json.dumps({"error": "Se ha producido un error"})
+        }
 
     return response
